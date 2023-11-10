d9c29c34d1cb9bf7f1a927ffde52a729c99e1ae6
Stephen Mutio Mutua <49070610+17g01acs028@users.noreply.github.com>
2023-11-10 11:02:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import multer from "multer";
 import helmet from "helmet";
-import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import AWS from "aws-sdk"
@@ -23,7 +22,6 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
