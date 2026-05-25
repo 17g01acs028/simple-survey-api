6e480ad7b80282dc7516fea8a76822f1342a40e1
17g01acs028 <stevemutioo123@gmail.com>
2026-05-25 12:39:58 +0300

    utl working

--- prisma/client/index.js
+++ prisma/client/index.js
@@ -1,12 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({
-    datasources: {
-      db: {
-        url: `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-      },
-    },
-  });
+const prisma = new PrismaClient();
   
 
 export { prisma };
